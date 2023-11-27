ba8921001b75058aa66a468d41dc3778b6057ad9
joelrajo <100616947+joelrajo@users.noreply.github.com>
2023-11-27 11:19:11 +0100

    Uniform Dist

--- computeGeometryUniform.m
+++ computeGeometryUniform.m
@@ -1,31 +1,40 @@
-function [yP, yC, deltaY,c,c12,theta,aoaE] = computeGeometryUniform(N,b,cR,cT,thetaT,aoa)
+function [coordsP,coordsC,deltaY,c,c12,theta,aoaE] = computeGeometryUniform(N,b,cR,cT,thetaT,aoa)
 
 
-yP     = zeros(N+1,1);
-yC     = zeros(N,1);
-deltaY = zeros(N,1);
+coordsP      = zeros(N+1,3);
+coordsC      = zeros(N,3);
+deltaY  = zeros(N,1);
 d      = b/N;
 sum    = -b/2;
 c12    = zeros(N,1);
 
+% Spanwise coordinates - Uniform Distribution
 for i=1:N
     if i == N
-        yP(i,1)   = sum;
-        yP(i+1,1) = sum + d;
-        yC(i,1)   = yP(i,1) + d/2;
+        coordsP(i,2)   = sum;
+        coordsP(i+1,2) = sum + d;
+        coordsC(i,2)   = coordsP(i,2) + d/2;
     else
-        yP(i,1)   = sum;
-        yC(i,1)   = yP(i,1) + d/2;
+        coordsP(i,2)   = sum;
+        coordsC(i,2)   = coordsP(i,2) + d/2;
         sum       = sum + d;
     end
     deltaY (i)    = d;
-
 end
-theta = [thetaT:-thetaT/(N/2):0,thetaT/(N/2):thetaT/(N/2):thetaT];
+
+% Torsion and Efective Angle of Attack
+if thetaT == 0
+    theta = zeros(1,N+1);
+else
+    theta = [thetaT:-thetaT/(N/2):0,thetaT/(N/2):thetaT/(N/2):thetaT];
+end
 aoaE  = theta + aoa; % Efective angle of attack
+
+% Chord length of each slice
 c     = [cT:(cR-cT)/(N/2):cR,cR-(cR-cT)/(N/2):-(cR-cT)/(N/2):cT];
 for i = 1:N
     c12(i) = (c(i)+c(i+1))/2;
 end
+
 end
 
